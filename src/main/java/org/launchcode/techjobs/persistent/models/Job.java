89f53cbbf9580a5d94b559e68abd30db067f8fe0
Aoekae <dark_aoekae@yahoo.com>
2022-11-21 09:34:05 -0600

    this works I promise

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,6 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 public class Job{
